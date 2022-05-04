e5d548aa263797e1fd60120cc9bf8928ad3d53ed
3arpbl3y <3arpbl3y@gmail.com>
2022-05-04 20:45:20 +0500

    quizData in js. basic html

---
+++ script.js
@@ -0,0 +1,34 @@
+const quizData = [
+  {
+    question: "bla bla bla?",
+    a: "11",
+    b: "12",
+    c: "13",
+    d: "14 ",
+    correct: "d",
+  },
+  {
+    question: "krya krya krya?",
+    a: "not krya",
+    b: "bla",
+    c: "blue",
+    d: "krya! ",
+    correct: "d",
+  },
+  {
+    question: "lol lol lol?",
+    a: "rip",
+    b: "ship",
+    c: "wha?",
+    d: "not enough lol ",
+    correct: "d",
+  },
+  {
+    question: "fr fr fer?",
+    a: "dig",
+    b: "d9og",
+    c: "cat?",
+    d: "fox ",
+    correct: "d",
+  },
+];
